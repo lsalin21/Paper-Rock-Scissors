6f5c6905b207987a30ba28714b97f8f1911c1e58
lsalin <lsalin21@gmail.com>
2021-11-19 23:41:17 -0600

    Added css file, added js code for project

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
-let computerScore = 0;
-let humanScore = 0;
-function 
+const choiceButtons = document.querySelectorAll('[data-selection]')
+const yourScoreSpan = document.querySelector('[data-computer-score]')
+const computerScoreSpan = document.querySelector('[data-your-score]')
+const selections =[
+    {
+        name:"rock",
+        beats:"scissors"
+    },
+    {
+        name:"paper",
+        beats:"rock"
+    },
+    {
+        name:"scissors",
+        beats:"paper"
+    }
+]
+
+choiceButtons.forEach(choiceButton => {
+ choiceButton.addEventListener('click', e => {
+    const choicesName = choiceButton.dataset.selection
+    const selection = selections.find(selection => selection.name === choicesName)
+    makeChoices(selection)
+ })
+})
+
+function makeChoices(selection){
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection , selection)
+
+    if(yourWinner)incrementScore(yourScoreSpan)
+    if(computerWinner)incrementScore(computerScoreSpan)
+}
+
+function incrementScore(scoreSpan){
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+function isWinner(selection,opponentsSelection){
+    return selection.beats === opponentsSelection.name
+}
+
+function randomSelection(){
+    const randomIndex = Math.floor(Math.random() * selections.length)
+    return selections[randomIndex]
+}
